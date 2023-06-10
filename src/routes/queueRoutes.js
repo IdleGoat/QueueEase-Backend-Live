33689b4facf2e2e1081508b98137379a441f797c
IdleGoat <rafie.amandio@ui.ac.id>
2023-06-10 22:14:15 +0700

    Fix io

--- src/routes/queueRoutes.js
+++ src/routes/queueRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { io } = require('../../app');
+const { io } = require('../../app').io;
 
 
 const queueController = require('../controllers/queueControllers');
